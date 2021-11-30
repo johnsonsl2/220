c68e4d4739a134c1a515b68f88eab55d869956c9
johnsonsl2 <johnsonsl2@g.cofc.edu>
2021-11-30 16:27:13 -0500

    Lab13

--- labs/lab12/algorithms.py
+++ labs/lab12/algorithms.py
@@ -31,14 +31,42 @@ def read_data(filename):
 
 def is_in_linear(search_val, value):
     #search_val is the value you want to find, value is a list of values
-    if value.count(str(search_val)) > 0:
-        return True
-    elif value.count(str(search_val)) == 0:
-        return False
+    for x in range(len(value)):
+        if value[x] == search_val:
+            return True
 
 
+def is_in_binary(search_val, values):
+    low = 0
+    high = len(values) - 1
+    while low <= high :
+        middle = (low + high) // 2
+        middle_value = values[middle]
+        if search_val == middle_value:
+            return middle
+        if search_val < middle_value:
+            high = middle - 1
+        if search_val > middle_value:
+            low = middle + 1
+    return False
 
+def selection_sort(values):
+    start = 0
+    while not start == len(values) - 1:
+        min = start
+        for i in range(start, len(values)):
+            if values[i] < values[min]:
+                min = i
+        move_num = values[start]
+        values[min] = values[start]
+        values[min] = move_num
+        start = start + 1
+    print(values)
 
 def main():
-    #is_in_linear()
+    is_in_linear()
+    is_in_binary()
+    selection_sort()
+
+
     pass
--- labs/lab13/lab13.py
+++ labs/lab13/lab13.py
@@ -1,10 +1,49 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Sara Johnson
+lab13.py
+
+Problem: Use while loops and list
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work
 """
+def is_in_binary(search_val, values):
+    low = 0
+    high = len(values) - 1
+    while low <= high :
+        middle = (low + high) // 2
+        middle_value = values[middle]
+        if search_val == middle_value:
+            return middle
+        if search_val < middle_value:
+            right = middle - 1
+        if search_val > middle_value:
+            left = middle + 1
+    return False
+
+def star_find(filename):
+    infile = open(filename, "r")
+    for line in infile.readlines():
+        num_list = line.split(" ")
+        occ = []
+    while len(occ) < 5:
+        for x in range(len(num_list)):
+            intn = int(num_list[x])
+            if intn >= 4000 and intn <= 5000:
+                occ.append(intn)
+    print("Five pulses were found:")
+    print(occ[0])
+    print(occ[1])
+    print(occ[2])
+    print(occ[3])
+    print(occ[4])
+    ind = num_list.index(str(occ[4]))
+    print("It took", ind, "searches to before finding the fifth signal")
+
 
 def main():
-    # add other function calls here
+    is_in_binary()
+    star_find("signals.txt")
     pass
 
 
